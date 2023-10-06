f46f7427bb05cafd8ef7737d5053599f60926a48
Ian Fisher - CGI <I.fisher@cgi.com>
2023-10-06 18:21:43 +0100

    Add keyvault

--- resource-group.tf
+++ resource-group.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "darts_resource_group" {
+resource "azurerm_resource_group" "juror_resource_group" {
   name     = format("%s-%s-rg", var.product, var.env)
   location = var.location
   tags = var.common_tags
