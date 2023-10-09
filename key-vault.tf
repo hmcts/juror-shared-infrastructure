4a077ecb6e38aa9a332ae9f378eba62c11bc4efd
Ian Fisher - CGI <I.fisher@cgi.com>
2023-10-09 14:34:18 +0100

    re-add kv secrets

--- key-vault.tf
+++ key-vault.tf
@@ -1,4 +1,4 @@
-module "juror-vault" {
+module "juror_vault" {
   source                     = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
   name                       = format("%s-%s", var.product, var.env)
   product                    = var.product
@@ -8,5 +8,5 @@ module "juror-vault" {
   resource_group_name        = azurerm_resource_group.juror_resource_group.name
   product_group_name         = "DTS Juror"
   common_tags                = var.common_tags
-  create_managed_identity    = false
+  create_managed_identity    = true
 }
