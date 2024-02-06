fb7e84296736d3023b689a5bdbc5ae4324e72693
Ian Fisher - CGI <I.fisher@cgi.com>
2024-02-06 20:36:08 +0000

    tweaked secters

--- secrets.tf
+++ secrets.tf
@@ -11,4 +11,27 @@ resource "azurerm_key_vault_secret" "public_jwtTTL" {
   key_vault_id = module.juror-vault.key_vault_id
 }
 
-#Now handle secrets
+#Now create secrets
+resource "random_password" "generated" {
+  for_each     = local.generated_secrets
+  length       = 32
+  lower        = true
+  upper        = true
+  numeric      = true
+  special      = false
+}
+
+resource "azurerm_key_vault_secret" "generated" {
+  for_each     = local.generated_secrets  
+  name         = "${each.value}"  
+  value        = random_password.generated[each.value].result 
+  key_vault_id = module.juror-vault.key_vault_id
+}
+
+#Now create base64 encoded version of secrets
+resource "azurerm_key_vault_secret" "encoded" {
+  for_each     = local.generated_secrets  
+  name         = "${each.value}-encoded"  
+  value        = base64encode(random_password.generated[each.value].result) 
+  key_vault_id = module.juror-vault.key_vault_id
+}
