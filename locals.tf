f2d5b1f848e8d957e0f788bcd31d480d1fe95bc7
Ian Fisher - CGI <I.fisher@cgi.com>
2024-02-07 17:03:13 +0000

    initial load of public and bureau secrets with matching api ones

--- locals.tf
+++ locals.tf
@@ -29,31 +29,41 @@ locals {
     "stg"  = "DTS Juror Admin (env:staging)"
     "prod" = "DTS Juror Admin (env:production)"
   }
-  fixed_secrets = {}
-  #Non secret parameters used by applications via secrets (instead of env vars) 
-  //fixed_secrets = {
-  //  "bureau-jwtTTL" = "8h",
-  //  "public-jwtTTL" = "8h"
-  //}
 
-  //generated_secrets = {
-  //   type = set(string)
-  //   default = [
-  //     "bureau-jwtKey",
-  //     "public-jwtKey",
-  //     "fe-jwtNoAuthKey",
-  //     "bureau-sessionSecret",
-  //     "public-sessionSecret",
-  //     "pnc-secret",
-  //     "scheduler-api-secret",
-  //     "scheduler-exe-secret"
-  //   ]
-  // }
+  #Non secret parameters which are the same across all environments, used by applications via secrets (instead of env vars) 
+  fixed_secrets = {
+    "bureau-jwtTTL" = "8h",
+    "public-jwtTTL" = "8h"
+  }
+
+  #Multi functional 'generated_secrets' block to cater for different scenarios - see examples below (first string isn't used, just needs to be unique):
+  #NOTES
+  #     first string isn't used, just needs to be unique - but also make sense
+  #     secret names in vault come from name, name2 and name64 parameters
+  #     secrets are min length 9 (because of min_upper, min_lower and min_numeric in secrets.tf)
+  
+  #  "Test-nothing" = { secret_length = 32, name = null, name2 = null, name64 = null }  
+  #  Will create a 32 character password but won't store it anywhere (so don't use)
+
+  #  "Test-normal" = { secret_length = 64, name = "Test-normal-only", name2 = null, name64 = null }
+  #  Will create a 64 character password and store the password as a secret named 'Test-normal-only'
+
+  #  "Test-64" = { secret_length = 32, name = null, name2 = null, name64 = "Test-64-only" }  
+  #  Will create a 32 character password and store the base64 encoded version of the password as a secret named 'Test-64-only'
+
+  #  "Test-both" = { secret_length = 32, name = "Test-normal-both", name2 = "Test-normal-both-Second", name64 = null }
+  #  Will create a 32 character password and store the password as secrets named 'Test-normal-both' and 'Test-normal-both-Second'
+
+  #  "Test-all" = { secret_length = 32, name = "Test-all", name2 = "Test-all-Second", name64 = "Test-all-64" }
+  #  Will create a 32 character password and store the password as secrets named 'Test-all' and 'Test-all-Second', 
+  #  plus it will store the base64 encoded version of the password as a secret named 'Test-all-64'
+  
+
   generated_secrets = {
-    "Ian-Test-none" = { secret_length = 32, name = null, name2 = null, name64 = null },    
-    "Ian-Test-normal-only" = { secret_length = 32, name = "Ian-Test-normal-only", name2 = null, name64 = null },
-    "Ian-Test-64-only" = { secret_length = 32, name = null, name2 = null, name64 = "Ian-Test-64-only" },    
-    "Ian-Test-normal-both" = { secret_length = 32, name = "Ian-Test-normal-both", name2 = "Ian-Test-normal-both-Second", name64 = null },
-    "Ian-Test-all" = { secret_length = 32, name = "Ian-Test-all", name2 = "Ian-Test-all-Second", name64 = "Ian-Test-all-64" }
+    "bureau-jwtKey" = { secret_length = 32, name = "bureau-jwtKey", name2 = null, name64 = "api-JWT-SECRET-BUREAU" },
+    "public-jwtKey" = { secret_length = 32, name = "public-jwtKey", name2 = null, name64 = "api-JWT-SECRET-PUBLIC" },
+    "jwtNoAuthKey" = { secret_length = 32, name = "bureau-jwtNoAuthKey", name2 = "public-jwtNoAuthKey", name64 = "api-JWT-SECRET-HMAC" },    
+    "bureau-sessionSecret" = { secret_length = 32, name = "bureau-sessionSecret", name2 = null, name64 = null },
+    "public-sessionSecret" = { secret_length = 32, name = "public-sessionSecret", name2 = null, name64 = null }
   }
 }
