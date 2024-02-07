8e2b5de9bb6f0217699657addc91e325ffa12988
Ian Fisher - CGI <I.fisher@cgi.com>
2024-02-07 12:20:19 +0000

    try conditional

--- secrets.tf
+++ secrets.tf
@@ -21,7 +21,7 @@ resource "azurerm_key_vault_secret" "stored" {
   for_each     = {
     for key, value in local.generated_secrets:
     key => value.store
-    if key == true
+    if key != false
   }  
   name         = each.key
   value        = random_password.generated[each.value].result 
@@ -30,11 +30,7 @@ resource "azurerm_key_vault_secret" "stored" {
 
 #Now create base64 encoded version of secrets and store if store_64 parameter is true
 resource "azurerm_key_vault_secret" "encoded" {
-  for_each     = {
-    for key, value in local.generated_secrets:
-    key => value.store_64
-    if key == true
-  } 
+  for_each     = local.generated_secrets
   name         = "${each.key}-encoded"  
   value        = base64encode(random_password.generated[each.value].result) 
   key_vault_id = module.juror-vault.key_vault_id
