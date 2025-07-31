71ac4eefb9255a87762aef61e0c27151db541a02
Neil Perry <142518199+npv0@users.noreply.github.com>
2025-07-31 15:24:47 +0100

    add application_insights audit_logs (#66)
    * add application_insights audit_logs
    
    * correct name
    
    * update to correct application insights categories
    
    * chore(deps): update terraform azurerm to v4
    
    * add location
    
    * use 4.x branch
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- app-insights.tf
+++ app-insights.tf
@@ -1,9 +1,10 @@
 module "application_insights" {
-  source = "git@github.com:hmcts/terraform-module-application-insights?ref=main"
+  source = "git@github.com:hmcts/terraform-module-application-insights?ref=4.x"
 
-  env     = var.env
-  product = var.product
-  name    = var.product
+  env      = var.env
+  product  = var.product
+  name     = var.product
+  location = var.location
 
   resource_group_name = azurerm_resource_group.juror_resource_group.name
 
@@ -26,4 +27,45 @@ resource "azurerm_key_vault_secret" "azure_appinsights_key" {
   name         = "AppInsightsInstrumentationKey"
   value        = module.application_insights.instrumentation_key
   key_vault_id = module.juror-vault.key_vault_id
+}
+
+resource "azurerm_monitor_diagnostic_setting" "ai-ds" {
+  name                       = "${var.product}-application_insights-${var.env}"
+  target_resource_id         = module.application_insights.id
+  log_analytics_workspace_id = module.log_analytics_workspace.workspace_id
+
+  enabled_log {
+    category = "AppRequests"
+  }
+
+  enabled_log {
+    category = "AppExceptions"
+  }
+
+  enabled_log {
+    category = "AppDependencies"
+  }
+
+  enabled_log {
+    category = "AppTraces"
+  }
+
+  enabled_log {
+    category = "AppAvailabilityResults"
+  }
+
+  enabled_log {
+    category = "AppPerformanceCounters"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      metric,
+    ]
+  }
+}
+
+module "log_analytics_workspace" {
+  source      = "git@github.com:hmcts/terraform-module-log-analytics-workspace-id.git?ref=master"
+  environment = var.env
 }
