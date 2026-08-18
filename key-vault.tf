3faec9497f51d20d30f507b65adc924af8806684
Neil Perry <142518199+npv0@users.noreply.github.com>
2026-08-18 14:47:42 +0100

    JS-971: update to new jenkins libary (#70)

--- key-vault.tf
+++ key-vault.tf
@@ -1,13 +1,20 @@
+data "azurerm_user_assigned_identity" "jenkins" {
+  name                = "jenkins-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
+}
+
 module "juror-vault" {
-  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  name                    = format("%s-%s", var.product, var.env)
-  product                 = var.product
-  env                     = var.env
-  tenant_id               = var.tenant_id
-  object_id               = var.jenkins_AAD_objectId
-  resource_group_name     = azurerm_resource_group.juror_resource_group.name
-  product_group_name      = "DTS Juror"
-  developers_group        = local.admin_group_map[var.env]
-  common_tags             = var.common_tags
-  create_managed_identity = true
+  source                   = "git@github.com:hmcts/cnp-module-key-vault?ref=DTSPO-31965/remove-jenkins-ptl-access"
+  name                     = format("%s-%s", var.product, var.env)
+  product                  = var.product
+  env                      = var.env
+  tenant_id                = var.tenant_id
+  object_id                = var.jenkins_AAD_objectId
+  resource_group_name      = azurerm_resource_group.juror_resource_group.name
+  product_group_name       = "DTS Juror"
+  developers_group         = local.admin_group_map[var.env]
+  common_tags              = var.common_tags
+  create_managed_identity  = true
+  grant_dev_jenkins_access = var.env == "stg"
+  jenkins_object_id        = data.azurerm_user_assigned_identity.jenkins.principal_id
 }
